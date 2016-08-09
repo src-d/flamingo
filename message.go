9aa62caec9d707da172a12af1e5442747ceed500
Miguel Molina <hi@mvader.me>
2016-08-09 10:37:37 +0200

    WIP: Remove attachments from user api

--- message.go
+++ message.go
@@ -3,19 +3,13 @@ package flamingo
 import "time"
 
 type OutgoingMessage struct {
-	ChannelID   string
-	Text        string
-	Sender      *MessageSender
-	Attachments []Attachment
+	ChannelID string
+	Text      string
+	Sender    *MessageSender
 }
 
 func NewOutgoingMessage(text string) OutgoingMessage {
 	return OutgoingMessage{Text: text}
-}
-
-func (m *OutgoingMessage) AddAttachment(client ClientType, att interface{}) *OutgoingMessage {
-	m.Attachments = append(m.Attachments, NewAttachment().Add(client, att))
-	return m
 }
 
 type MessageSender struct {
@@ -23,49 +17,14 @@ type MessageSender struct {
 	IconURL  string
 }
 
-type attachment struct {
-	byClient map[ClientType]interface{}
-}
-
-func NewAttachment() Attachment {
-	return &attachment{
-		byClient: make(map[ClientType]interface{}),
-	}
-}
-
-type Attachment interface {
-	Clients() []ClientType
-	ForClient(ClientType) (interface{}, bool)
-	Add(ClientType, interface{}) Attachment
-}
-
-func (a *attachment) ForClient(client ClientType) (interface{}, bool) {
-	att, ok := a.byClient[client]
-	return att, ok
-}
-
-func (a *attachment) Add(client ClientType, attachment interface{}) Attachment {
-	a.byClient[client] = attachment
-	return a
-}
-
-func (a *attachment) Clients() []ClientType {
-	clients := make([]ClientType, 0, len(a.byClient))
-	for t := range a.byClient {
-		clients = append(clients, t)
-	}
-	return clients
-}
-
 type Message struct {
-	ID          string
-	Type        ClientType
-	User        User
-	Channel     Channel
-	Time        time.Time
-	Attachments []Attachment
-	Text        string
-	Extra       interface{}
+	ID      string
+	Type    ClientType
+	User    User
+	Channel Channel
+	Time    time.Time
+	Text    string
+	Extra   interface{}
 }
 
 type Conversation []OutgoingMessage
--- slack/message.go
+++ slack/message.go
@@ -1,7 +1,6 @@
 package slack
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -10,65 +9,14 @@ import (
 	"github.com/nlopes/slack"
 )
 
-type Attachment struct {
-	Color         string
-	AuthorName    string
-	AuthorSubname string
-	AuthorLink    string
-	AuthorIcon    string
-
-	Title     string
-	TitleLink string
-	Pretext   string
-	Text      string
-
-	ImageURL string
-	ThumbURL string
-
-	Fields     []Field
-	ID         string
-	Actions    []Action
-	MarkdownIn []string
-
-	Footer     string
-	FooterIcon string
-}
-
-type Field struct {
-	Title string
-	Value string
-	Short bool
-}
-
-type Action struct {
-	Name    string
-	Text    string
-	Value   string
-	Style   string
-	Confirm []Confirmation
-}
-
-type Confirmation struct {
-	Title       string
-	Text        string
-	OkText      string
-	DismissText string
-}
-
 func newMessage(user flamingo.User, channel flamingo.Channel, src slack.Msg) flamingo.Message {
-	var attachments = make([]flamingo.Attachment, 0, len(src.Attachments))
-	for _, a := range src.Attachments {
-		attachments = append(attachments, convertAttachment(a))
-	}
-
 	return flamingo.Message{
-		User:        user,
-		Type:        flamingo.SlackClient,
-		Channel:     channel,
-		Text:        src.Text,
-		Time:        parseTimestamp(src.Timestamp),
-		Attachments: attachments,
-		Extra:       src,
+		User:    user,
+		Type:    flamingo.SlackClient,
+		Channel: channel,
+		Text:    src.Text,
+		Time:    parseTimestamp(src.Timestamp),
+		Extra:   src,
 	}
 }
 
@@ -94,115 +42,5 @@ func createPostParams(botID, channelID string, msg flamingo.OutgoingMessage) (sl
 		params.Username = msg.Sender.Username
 	}
 
-	for _, a := range msg.Attachments {
-		if att, ok := a.ForClient(flamingo.SlackClient); ok {
-			slackAtt, ok := att.(Attachment)
-			if !ok {
-				return params, fmt.Errorf("attachment %#v is not of type slack.Attachment", att)
-			}
-
-			params.Attachments = append(params.Attachments, toSlackAttachment(botID, channelID, slackAtt))
-		}
-	}
-
 	return params, nil
-}
-
-func toSlackAttachment(botID, channelID string, attachment Attachment) slack.Attachment {
-	a := slack.Attachment{
-		CallbackID:    fmt.Sprintf("%s::%s::%s", botID, channelID, attachment.ID),
-		Color:         attachment.Color,
-		AuthorName:    attachment.AuthorName,
-		AuthorSubname: attachment.AuthorSubname,
-		AuthorLink:    attachment.AuthorLink,
-		AuthorIcon:    attachment.AuthorIcon,
-		Title:         attachment.Title,
-		TitleLink:     attachment.TitleLink,
-		Pretext:       attachment.Pretext,
-		Text:          attachment.Text,
-		ImageURL:      attachment.ImageURL,
-		ThumbURL:      attachment.ThumbURL,
-		Footer:        attachment.Footer,
-		FooterIcon:    attachment.FooterIcon,
-		MarkdownIn:    attachment.MarkdownIn,
-	}
-
-	for _, f := range attachment.Fields {
-		a.Fields = append(a.Fields, slack.AttachmentField{
-			Title: f.Title,
-			Value: f.Value,
-			Short: f.Short,
-		})
-	}
-
-	for _, action := range attachment.Actions {
-		act := slack.AttachmentAction{
-			Name:  action.Name,
-			Text:  action.Text,
-			Value: action.Value,
-			Style: action.Style,
-			Type:  "button",
-		}
-
-		for _, c := range action.Confirm {
-			act.Confirm = append(act.Confirm, slack.ConfirmationField{
-				Title:       c.Title,
-				Text:        c.Text,
-				OkText:      c.OkText,
-				DismissText: c.DismissText,
-			})
-		}
-		a.Actions = append(a.Actions, act)
-	}
-
-	return a
-}
-
-func convertAttachment(attachment slack.Attachment) flamingo.Attachment {
-	a := Attachment{
-		Color:         attachment.Color,
-		AuthorName:    attachment.AuthorName,
-		AuthorSubname: attachment.AuthorSubname,
-		AuthorLink:    attachment.AuthorLink,
-		AuthorIcon:    attachment.AuthorIcon,
-		ID:            attachment.CallbackID,
-		Title:         attachment.Title,
-		TitleLink:     attachment.TitleLink,
-		Pretext:       attachment.Pretext,
-		Text:          attachment.Text,
-		ImageURL:      attachment.ImageURL,
-		ThumbURL:      attachment.ThumbURL,
-		Footer:        attachment.Footer,
-		FooterIcon:    attachment.FooterIcon,
-		MarkdownIn:    attachment.MarkdownIn,
-	}
-
-	for _, f := range attachment.Fields {
-		a.Fields = append(a.Fields, Field{
-			Title: f.Title,
-			Value: f.Value,
-			Short: f.Short,
-		})
-	}
-
-	for _, action := range attachment.Actions {
-		act := Action{
-			Name:  action.Name,
-			Text:  action.Text,
-			Value: action.Value,
-			Style: action.Style,
-		}
-
-		for _, c := range action.Confirm {
-			act.Confirm = append(act.Confirm, Confirmation{
-				Title:       c.Title,
-				Text:        c.Text,
-				OkText:      c.OkText,
-				DismissText: c.DismissText,
-			})
-		}
-		a.Actions = append(a.Actions, act)
-	}
-
-	return flamingo.NewAttachment().Add(flamingo.SlackClient, a)
 }
